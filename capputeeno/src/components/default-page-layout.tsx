c88374b854440d1330034819dbca75f5879d24dd
alvimm <filipe2012alvim@gmail.com>
2024-01-04 18:55:19 -0300

    fix: fixing the height of default page

--- capputeeno/src/components/default-page-layout.tsx
+++ capputeeno/src/components/default-page-layout.tsx
@@ -4,10 +4,11 @@ import styled from "styled-components"
 
 export const DefaultPageLayout = styled.div`
     padding: 12px 24px;
-    min-height: 100vh;
+    min-height: calc(100vh - 33px - 33px);
     background-color: var(--bg-primary);
 
     @media(min-width: ${props => props.theme.desktopBreakpoint}){
     padding: 34px 160px;
+    min-height: calc(100vh - 50px - 50px);
   }
   `
